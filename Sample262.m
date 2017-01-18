f1b03f2b7ff2041b19f7286e1ab4d4cdf037b1f9
ih64 <imran.hasan.ih@gmail.com>
2017-01-18 00:14:54 -0800

    modified Sample262: scaled par values by 2. creaded g* files and hw1Prob2.m to create the *.png figures

--- Sample262.m
+++ Sample262.m
@@ -6,20 +6,21 @@
 % the data structure "par" contains all the fixed paramters
 % I reccomend using data structures so that all paramters can easily be
 % passed to functions.
-par.m = 1; % Note: If you do not put a semicolon at the end of a line the result will appear on the command line
-par.b = 4;  
+par.m = 2; % Note: If you do not put a semicolon at the end of a line the result will appear on the command line
+par.b = 8;  
 
-par.a = 3;
-par.c = -1;
+par.a = 6;
+par.c = -2;
 
-par.A = 1; %matlab is case sensitive
-par.f = 2*pi; % Note that pi is predifined in Matlab;
+par.A = 2; %matlab is case sensitive
+par.f = 4*pi; % Note that pi is predifined in Matlab;
 
 x = linspace(0,1) % define a vector x to be the x coordinates
 y1 = f1(x,par); % define vector of y coordinates
 y2 = f2(x,par); % define another vector of y coordinates
 y3 = fSin(x,par); % define another vector of y coordinates
 
+f=figure()
 plot(x,y1,'r');  % type "help plot" at the command line for more info
 hold on % this command causes the next plot to appear in the same figure
 plot(x,y2,'b');
@@ -27,9 +28,11 @@ plot(x,y3,'k');
 hold off
 
 xlim([0,1]); % set axis limits
-ylim([-3, 7]);
+ylim([-3, 10]);
 
 xlabel('x axis')
 ylabel('y axis')
 title('Title')
 legend('linear','quadratic','sine')
+saveas(f,'figure1.png')
+
